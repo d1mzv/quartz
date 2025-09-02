ceaf075a7c137dd1d4be97d9811b1e618fb27eaa
d1mzv <dmitry.maznev@gmail.com>
2025-09-02 04:20:32 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://dmzv.me/kb",
+    baseUrl: "https://kb.dmzv.me/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
